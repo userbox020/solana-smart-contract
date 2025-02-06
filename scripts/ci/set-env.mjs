ea9f10b147879efc447a59d07305417ead5b1fb2
Mr.Userbox <01.leonvaldez@gmail.com>
2025-02-05 23:06:31 -0600

    Update 67v7JZ

--- scripts/ci/set-env.mjs
+++ scripts/ci/set-env.mjs
@@ -5,3 +5,4 @@ await $`echo "ANCHOR_VERSION=${getCargoMetadata()?.cli?.anchor}" >> $GITHUB_ENV`
 await $`echo "SOLANA_VERSION=${getSolanaVersion()}" >> $GITHUB_ENV`;
 await $`echo "TOOLCHAIN_FORMAT=${getToolchain('format')}" >> $GITHUB_ENV`;
 await $`echo "TOOLCHAIN_LINT=${getToolchain('lint')}" >> $GITHUB_ENV`;
+
