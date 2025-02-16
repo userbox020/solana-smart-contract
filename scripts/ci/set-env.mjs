6b97e96614b17d17c0a5aabc99bac493216c06de
Mr.Userbox <01.leonvaldez@gmail.com>
2025-02-15 21:13:33 -0600

    Update 3h09Zr

--- scripts/ci/set-env.mjs
+++ scripts/ci/set-env.mjs
@@ -6,3 +6,4 @@ await $`echo "SOLANA_VERSION=${getSolanaVersion()}" >> $GITHUB_ENV`;
 await $`echo "TOOLCHAIN_FORMAT=${getToolchain('format')}" >> $GITHUB_ENV`;
 await $`echo "TOOLCHAIN_LINT=${getToolchain('lint')}" >> $GITHUB_ENV`;
 
+
